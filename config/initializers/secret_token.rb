9df74808e9f77117f61eb267e6796e62253ef256
aleflore <aleflore@mail.csuchico.edu>
2013-11-14 22:36:38 -0800

    all but the photos commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Photoapp::Application.config.secret_key_base = '2b90bc586555c878c6ea97680f40c376bc7e857b439d241df2d65617fdcb8b42b4f663a47b6e04eb81545cd808d85244b3d1d1e8ce49b4bce5065a04e0cc5190'
